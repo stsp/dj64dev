d644f56e26f8913e24ef762e6cd6b83736c47b5f
Stas Sergeev <stsp@users.sourceforge.net>
2024-10-23 12:44:41 +0300

    memcpy/memmove/memset from musl [#15]

---
+++ src/libc/ansi/string/memcpy.c
@@ -0,0 +1,12 @@
+#include <string.h>
+#include <stdint.h>
+#include <endian.h>
+
+void *memcpy(void *restrict dest, const void *restrict src, size_t n)
+{
+	unsigned char *d = dest;
+	const unsigned char *s = src;
+
+	for (; n; n--) *d++ = *s++;
+	return dest;
+}
