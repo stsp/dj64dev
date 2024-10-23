f7ce1eac59815a6b3d889b279d16f0f95d819dff
Stas Sergeev <stsp@users.sourceforge.net>
2024-10-23 23:55:27 +0300

    dummy llrintl w/o inline asm

--- src/libc/c99/math/llrintl.c
+++ src/libc/c99/math/llrintl.c
@@ -1,14 +1,6 @@
-/* Copyright (C) 2013 DJ Delorie, see COPYING.DJ for details */
-
 #include <math.h>
 
-
-long long int
-llrintl(long double x)
+long long llrintl(long double x)
 {
-  long long int result;
-
-  asm("fistpll %0" : "=m" (result) : "t" (x) : "st");
-
-  return result;
+	return rintl(x);
 }
