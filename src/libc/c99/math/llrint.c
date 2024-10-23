5fa88529e70deeabd45973db02c729641975dede
Stas Sergeev <stsp@users.sourceforge.net>
2024-10-23 23:46:19 +0300

    dummy llrint w/o inline asm

--- src/libc/c99/math/llrint.c
+++ src/libc/c99/math/llrint.c
@@ -1,14 +1,8 @@
-/* Copyright (C) 2013 DJ Delorie, see COPYING.DJ for details */
-
 #include <math.h>
 
+/* uses LLONG_MAX > 2^53, see comments in lrint.c */
 
-long long int
-llrint(double x)
+long long llrint(double x)
 {
-  long long int result;
-
-  asm("fistpll %0" : "=m" (result) : "t" (x) : "st");
-
-  return result;
+	return rint(x);
 }
