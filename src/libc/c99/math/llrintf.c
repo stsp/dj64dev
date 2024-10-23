c3306eb88013925f01826dffd164d76c0d745ba2
Stas Sergeev <stsp@users.sourceforge.net>
2024-10-23 23:50:35 +0300

    dummy llrintf w/o inline asm

--- src/libc/c99/math/llrintf.c
+++ src/libc/c99/math/llrintf.c
@@ -1,14 +1,8 @@
-/* Copyright (C) 2013 DJ Delorie, see COPYING.DJ for details */
-
 #include <math.h>
 
+/* uses LLONG_MAX > 2^24, see comments in lrint.c */
 
-long long int
-llrintf(float x)
+long long llrintf(float x)
 {
-  long long int result;
-
-  asm("fistpll %0" : "=m" (result) : "t" (x) : "st");
-
-  return result;
+	return rintf(x);
 }
