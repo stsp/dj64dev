3e02ef616532e173fe2ba90b5477c59335702fa3
Stas Sergeev <stsp@users.sourceforge.net>
2024-12-13 16:16:20 +0300

    stdarg: always use latest builtins

--- include/stdarg.h
+++ include/stdarg.h
@@ -22,40 +22,10 @@ __DJ_va_list
 #define _VA_LIST
 #endif
 
-/* For GCC-4.4.X the definitions have chenged again ...                    */
-/* This is however not surprising as we are messing here with internal     */
-/* stuff of GCC.                                                           */
-#if ((__GNUC__ == 4) && (__GNUC_MINOR__ >= 4)) || (__GNUC__ > 4)
-
 #define va_start(ap, last_arg)   __builtin_va_start(ap,last_arg)
 #define va_end(ap)       __builtin_va_end(ap)
 #define va_arg(v, l)     __builtin_va_arg(v,l)
 #define va_copy(dst, src)   __builtin_va_copy(dst,src)
-
-/* For GCC 2.96 or later we use its builtin va_list                        */
-/* In most cases these definitions won't be used, because headers provided */
-/* by GCC hide them. But we can DTRT if GCC headers are missing.           */
-#elif ((__GNUC__ == 2) && (__GNUC_MINOR__ >= 96)) || (__GNUC__ >= 3)
-
-#define va_arg                  __builtin_va_arg
-#define va_end                  __builtin_va_end
-#define va_start(ap, last_arg)  __builtin_stdarg_start((ap), (last_arg))
-
-#else /* #if ((__GNUC__ == 2) && (__GNUC_MINOR__ >= 96)) || (__GNUC__ >= 3) */
-
-#define __dj_va_rounded_size(T)  \
-  (((sizeof (T) + sizeof (int) - 1) / sizeof (int)) * sizeof (int))
-
-#define va_arg(ap, T) \
-    (ap = (va_list) ((char *) (ap) + __dj_va_rounded_size (T)),	\
-     *((T *) (void *) ((char *) (ap) - __dj_va_rounded_size (T))))
-
-#define va_end(ap)
-
-#define va_start(ap, last_arg) \
- (ap = ((va_list) __builtin_next_arg (last_arg)))
-
-#endif /* #if ((__GNUC__ == 2) && (__GNUC_MINOR__ >= 96)) || (__GNUC__ >= 3) */
 
 #if (defined(__STDC_VERSION__) && __STDC_VERSION__ >= 199901L) \
   || !defined(__STRICT_ANSI__) || defined(__cplusplus)
