67575718a5319c1b69731548abb7c96e67927277
Stas Sergeev <stsp@users.sourceforge.net>
2024-12-21 02:36:47 +0300

    sanitize_arch.sh: remove .addrsig added by clang

--- src/libc/dj64/parsers/sanitize_arch.sh
+++ src/libc/dj64/parsers/sanitize_arch.sh
@@ -1,6 +1,6 @@
 ARCH=`head -n 1 $1 | grep .arch | cut -d " " -f 2`
 if [ -z "$ARCH" ]; then
-  cat $1
+  cat $1 | grep -v .addrsig
   exit 0
 fi
-cat $1 | grep -v .arch | sed 's/.align/.p2align/g'
+cat $1 | grep -v .arch | grep -v .addrsig | sed 's/.align/.p2align/g'
