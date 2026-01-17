a0c90a6a6aacb46a0c5d36df83ee9973074ae5fe
Stas Sergeev <stsp@users.sourceforge.net>
2026-01-17 18:08:30 +0200

    sanitize_arch.sh: dont use cat

--- src/libc/dj64/parsers/sanitize_arch.sh
+++ src/libc/dj64/parsers/sanitize_arch.sh
@@ -1,7 +1,7 @@
 F_STRS=\\.arch\|\\.addrsig\|\\.fpu\|\\.eabi_attribute
 ARCH=`grep .arch $1 | cut -d " " -f 2`
 if [ -z "$ARCH" ]; then
-  cat $1 | grep -E -v $F_STRS
+  grep -E -v $F_STRS $1
   exit 0
 fi
-cat $1 | grep -E -v $F_STRS | sed 's/.align/.p2align/g'
+grep -E -v $F_STRS $1 | sed 's/.align/.p2align/g'
