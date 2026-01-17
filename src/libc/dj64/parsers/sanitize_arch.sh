feb4f6aaf699f7413c3245fbe4a57f94d2e68501
Stas Sergeev <stsp@users.sourceforge.net>
2026-01-17 18:01:24 +0200

    sanitize_arch.sh: escape dots [#51]

--- src/libc/dj64/parsers/sanitize_arch.sh
+++ src/libc/dj64/parsers/sanitize_arch.sh
@@ -1,4 +1,4 @@
-F_STRS=.arch\|.addrsig\|.fpu\|.eabi_attribute
+F_STRS=\\.arch\|\\.addrsig\|\\.fpu\|\\.eabi_attribute
 ARCH=`head -n 1 $1 | grep .arch | cut -d " " -f 2`
 if [ -z "$ARCH" ]; then
   cat $1 | grep -E -v $F_STRS
