cdaddda21427211ce0b9490011e40e7aee9c49b5
Stas Sergeev <stsp@users.sourceforge.net>
2026-03-13 21:30:15 +0200

    puts: another CR fix
    This time, check if the file is binary, as for text files CRs
    are inserted elsewhere.
    
    Fixes https://github.com/dosemu2/comcom64/issues/96

--- src/libc/ansi/stdio/puts.c
+++ src/libc/ansi/stdio/puts.c
@@ -1,5 +1,6 @@
 /* Copyright (C) 1994 DJ Delorie, see COPYING.DJ for details */
 #include <stdio.h>
+#include "libc/file.h"
 
 int
 puts(const char *s)
@@ -8,5 +9,7 @@ puts(const char *s)
 
   while ((c = *s++))
     putchar(c);
+  if (!__is_text_file(stdout))
+    putchar('\r');
   return putchar('\n');
 }
