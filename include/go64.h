81d7476ae87918d8136e90bf705378f78ffc02b6
Stas Sergeev <stsp@users.sourceforge.net>
2026-06-18 11:11:18 +0300

    go64.h: ifdef 64bit ELF loaders [#58]
    They are unavailable in 32bit mode.

--- include/go64.h
+++ include/go64.h
@@ -1,6 +1,8 @@
 #ifndef GO64_H
 #define GO64_H
 
+#if defined(DJ64) && !defined(DJ32)
+
 void pltcall32(__dpmi_regs *regs, __dpmi_paddr addr);
 void pltctrl32(__dpmi_regs *regs, int fn, int arg);
 void upltinit32(__dpmi_regs *regs);
@@ -8,5 +10,7 @@ int elfexec(const char *path, int argc, char **argv);
 int elfload(int num);
 
 #define ELFEXEC_LIBID 2
+
+#endif
 
 #endif
