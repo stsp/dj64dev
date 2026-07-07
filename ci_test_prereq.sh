9458c8406f2d1d878843829e47c268d7aa00b28f
Stas Sergeev <stsp@users.sourceforge.net>
2026-07-07 23:05:54 +0300

    ci: run autotests on demos
    This covers quite a lot of stub functionality, which was
    exceptionally buggy all the time. That needs to be changed.

--- ci_test_prereq.sh
+++ ci_test_prereq.sh
@@ -2,4 +2,5 @@
 
 sudo add-apt-repository ppa:stsp-0/dj64
 sudo add-apt-repository ppa:dosemu2/ppa
-sudo apt install -y djstub dj64-host dos2unix
+sudo add-apt-repository -y ppa:jwt27/djgpp-toolchain
+sudo apt install -y djstub dj64-host dos2unix gcc-djgpp djgpp-dev
